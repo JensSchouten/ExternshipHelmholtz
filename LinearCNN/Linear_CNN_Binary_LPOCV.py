caccaf72ed40744197498bb785f81ad980018d8b
JensSchouten <45451881+JensSchouten@users.noreply.github.com>
2019-01-10 13:23:08 +0100

    Create Linear_CNN_Binary_LPOCV.py

---
+++ LinearCNN/Linear_CNN_Binary_LPOCV.py
@@ -0,0 +1,204 @@
+### IMPORT PACKAGES
+import numpy as np
+from datetime import datetime
+import os
+import pandas as pd
+from skimage import io
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+from keras import layers, models
+import pickle
+from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger
+import random
+
+### INPUT
+## Data augmentation? Yes(1), no(0)
+data_augmentation = 1  
+
+## Paths
+path_to_images = ""     # To images
+path_to_labels = ""     # To labels
+label_column = ""       # Column of right labels
+path_to_save = ""       # To save folder
+
+## Model name
+modelname = ""
+
+## Number of epochs
+epoch_nr = 500
+
+## Image dimensions
+img_height = 257
+img_width = 257
+img_channels = 3
+output = 1
+
+
+### MODEL SETUP
+def setup_sequential_model():
+        model = models.Sequential()
+        
+        model.add(layers.Conv2D(4, (3, 3), activation='relu', input_shape=input_shape))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(8, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(8, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(8, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        
+        model.add(layers.Flatten())
+        model.add(layers.Dense(32, activation='relu'))
+        model.add(layers.Dense(output, activation='sigmoid'))
+
+        return model
+
+
+if K.image_data_format() == 'channels_first':
+        input_shape = (img_channels, img_width, img_height)
+else:
+        input_shape = (img_width, img_height, img_channels)
+
+
+### CREATE DATA GENERATORS
+test_datagen = ImageDataGenerator(rescale=1./255)
+if data_augmentation == 1:
+    train_datagen = ImageDataGenerator(
+            rotation_range=359,
+            horizontal_flip=True,
+            vertical_flip=True,
+            rescale=1./255,    
+            fill_mode='nearest')        
+elif data_augmentation == 0:
+    train_datagen = ImageDataGenerator(rescale=1./255)
+
+
+### IMPORT IMAGES
+pathIm, dirsIm, filesIm = next(os.walk(path_to_images))
+        
+## Put all images in a 3D matrix and save image numbers
+imagesAll = []    
+imageNrs = []
+for i in range(len(filesIm)):
+    im = io.imread(pathIm + filesIm[i],0) 
+    im = im[:,:,0:3]
+    imagesAll.append(im)
+    imageNrs.append(int(filesIm[i][2:5])-1)
+    
+imagesAll = np.stack(imagesAll,axis=0)  
+
+
+### IMPORT LABELS
+df = pd.read_excel(path_to_labels)
+
+labelsAll = df[[label_column]].values.flatten()
+labelsAll = labelsAll[imageNrs]
+
+
+### SHUFFLE IMAGES AND LABELS TO GET RANDOM DISTRIBUTION
+indices = list(range(len(labelsAll)))
+random.shuffle(indices)
+
+labelsAll = labelsAll[indices]
+imagesAll = imagesAll[indices,:,:]
+
+
+### TRAINING AND TESTING FOR EACH FOLD
+compares = []
+for CV in range(0,len(labelsAll),5):
+    
+    ### CREATE NEW NETWORK
+    model = setup_sequential_model()
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+    
+    
+    ### CREATE IMAGE AND LABEL SETS
+    ## Create the testing images and labels
+    idxTest = list(range(CV,CV+5))
+    imagesTest = imagesAll[idxTest,:,:]
+    labelsTest = labelsAll[idxTest]
+    
+    ## Create the validation images and labels
+    idxTrain = [i for j, i in enumerate(list(range(len(labelsAll)))) if j not in idxTest]    
+    
+    idxVal = random.sample(idxTrain, 45)   
+    imagesVal = imagesAll[idxVal,:,:]
+    labelsVal = labelsAll[idxVal]
+    
+    ## Redefine training images and labels 
+    idxTrain_new = [i for j, i in enumerate(list(range(len(labelsAll)))) if j not in idxVal and j not in idxTest]
+    
+    imagesTrain = imagesAll[idxTrain_new,:,:]
+    labelsTrain = labelsAll[idxTrain_new]   
+    
+    
+    ### TRAIN NETWORK
+    ## Create generators
+    train_generator = train_datagen.flow(
+            imagesTrain, labelsTrain,
+            batch_size=5,
+            shuffle=False)
+    
+    val_generator = test_datagen.flow(
+            imagesVal, labelsVal,
+            batch_size=5,
+            shuffle=False)
+    
+    ## Use of callbacks
+    checkpoint = ModelCheckpoint(path_to_save + modelname + '_' + str(CV) + '_checkpoint' + '.hdf5', monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+    csvlog = CSVLogger(path_to_save+modelname+ '_' + str(CV) + '_train_log.csv',append=False)
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=30, verbose=1)
+    
+    ## Training
+    startTime = datetime.now()    
+    history = model.fit_generator(
+                train_generator,
+                steps_per_epoch=40,
+                epochs=epoch_nr,
+                validation_data=val_generator,
+                validation_steps=10,
+                callbacks = [checkpoint, csvlog, early_stopping])    
+    Time = datetime.now() - startTime
+
+      
+    ### TESTING THE BEST NETWORK OF THIS FOLD     
+    ## Load best network
+    model = setup_sequential_model()
+    model = load_model(path_to_save + modelname+ '_' + str(CV) + '_checkpoint' + '.hdf5')
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['acc'])
+        
+    ## Create generator
+    test_generator = test_datagen.flow(
+                imagesTest, labelsTest,
+                batch_size=1,
+                shuffle=False)    
+        
+    ## Testing the network
+    predictions = model.predict_generator(
+            test_generator, 
+            steps=len(labelsTest),
+            verbose=1)
+
+    ## Create array with labels and predictions
+    compare = np.column_stack((labelsTest,predictions))
+    compares.extend(compare)
+
+    ### SAVING VARIABLES OF FOLD
+    with open(path_to_save + modelname + '_' + str(CV) + '.pkl', 'wb') as f:
+        pickle.dump([Time, compare, idxTest, idxTrain_new, idxVal], f)
+    
+    
+### SAVING RESULTS
+compares = np.stack(compares,axis=0)  
+with open(path_to_save + modelname + '.pkl', 'wb') as f:
+    pickle.dump([compares], f)
+
